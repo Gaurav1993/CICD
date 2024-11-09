5f1da19a29b965622da370dbe0f850f1ff8522ec
Gaurav <gaurav.singh881@gmail.com>
2024-11-09 19:11:26 +0530

    ninteeCommit

--- app/src/androidTest/java/com/example/kotlin_project/TestUiSaveData.kt
+++ app/src/androidTest/java/com/example/kotlin_project/TestUiSaveData.kt
@@ -1,17 +1,17 @@
-package com
+package com.example.kotlin_project
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.kotlin_project.MainActivity
-import com.example.kotlin_project.R
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matchers.*
 
 @RunWith(AndroidJUnit4::class)
 class TestUiSaveData
@@ -29,7 +29,11 @@ class TestUiSaveData
         onView(withId(R.id.et_email)).perform(typeText("s@gmail.com"), closeSoftKeyboard())
         onView(withId(R.id.et_password)).perform(typeText("123456"), closeSoftKeyboard())
         onView(withId(R.id.et_mobile)).perform(typeText("1234567890"), closeSoftKeyboard())
+        onView(isRoot()).perform(closeSoftKeyboard())
+
         onView(withId(R.id.bt_save)).perform(click())
+
+
     }
 
     @Test
@@ -39,6 +43,14 @@ class TestUiSaveData
         onView(withId(R.id.et_email)).perform(typeText("invalid"), closeSoftKeyboard())
         onView(withId(R.id.et_password)).perform(typeText("short"), closeSoftKeyboard())
         onView(withId(R.id.et_mobile)).perform(typeText("123"), closeSoftKeyboard())
+
+        onView(isRoot()).perform(closeSoftKeyboard())
         onView(withId(R.id.bt_save)).perform(click())
+
+//        onView(withId(R.id.et_name)).check(matches(hasErrorText("Name is required")))
+//        onView(withId(R.id.et_email)).check(matches(hasErrorText("Invalid email address")))
+//        onView(withId(R.id.et_password)).check(matches(hasErrorText("Password is too short")))
+//        onView(withId(R.id.et_mobile)).check(matches(hasErrorText("Invalid phone number")))
+
     }
 }
